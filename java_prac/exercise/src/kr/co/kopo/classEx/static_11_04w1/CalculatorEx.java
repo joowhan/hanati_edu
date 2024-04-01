fd4416204b125a94e68c39e2077ab9c9398ccfef
joowhan <joy980721@gmail.com>
2024-04-01 14:29:38 +0900

    [java] insert code > static 240401

---
+++ java_prac/exercise/src/kr/co/kopo/classEx/static_11_04w1/CalculatorEx.java
@@ -0,0 +1,16 @@
+package kr.co.kopo.classEx.static_11_04w1;
+
+public class CalculatorEx {
+
+    public static void main(String[] args){
+
+        double result1 = 10*10*Calculator.pi;
+        int result2 = Calculator.plus(10,5);
+        int result3 = Calculator.minus(10,5);
+        Calculator.println(10);
+        Calculator.println(true);
+        Calculator.println(5.7);
+        Calculator.println("홍길동");
+
+    }
+}
