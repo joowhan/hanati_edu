14607289d6dcdfff554d0cdcf6db907b22cfd44a
김주환 <joy980721@gmail.com>
2024-04-07 23:47:47 +0900

    [java] fix dir > java week2 assignment submit 240407

---
+++ java_prac/week2_java/src/TbContent.java
@@ -0,0 +1,58 @@
+public class TbContent {
+    private String idFile;
+    private String nmOrgFile;
+
+    private String boSaveFile;
+
+    private String nmFileExt;
+    private String daSave;
+    private int cnHit;
+
+    public String getIdFile() {
+        return idFile;
+    }
+
+    public String getNmOrgFile() {
+        return nmOrgFile;
+    }
+
+    public String getBoSaveFile() {
+        return boSaveFile;
+    }
+
+    public String getNmFileExt() {
+        return nmFileExt;
+    }
+
+    public String getDaSave() {
+        return daSave;
+    }
+
+    public int getCnHit() {
+        return cnHit;
+    }
+
+    public void setIdFile(String idFile) {
+        this.idFile = idFile;
+    }
+
+    public void setNmOrgFile(String nmOrgFile) {
+        this.nmOrgFile = nmOrgFile;
+    }
+
+    public void setBoSaveFile(String boSaveFile) {
+        this.boSaveFile = boSaveFile;
+    }
+
+    public void setNmFileExt(String nmFileExt) {
+        this.nmFileExt = nmFileExt;
+    }
+
+    public void setDaSave(String daSave) {
+        this.daSave = daSave;
+    }
+
+    public void setCnHit(int cnHit) {
+        this.cnHit = cnHit;
+    }
+}
